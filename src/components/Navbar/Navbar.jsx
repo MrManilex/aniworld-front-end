1a922de8ef11719e2cc5075c92f4816b8ffbcbfb
UbuntuLex <alexandermaniwang99@gmail.com>
2022-08-29 15:40:31 -0400

    fix tailwind config and change theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">AniWorld</a>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
