ae7a9801d8f39e798f0c139ac0b9d4dac8adf75d
UbuntuLex <alexandermaniwang99@gmail.com>
2022-08-29 17:20:47 -0400

    add navigate method to direct users straight to home page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { searchAnimes } from '../../services/animeService';
 import Navbar from '../../components/Navbar/Navbar';
 import AnimeSearch from "../../components/AnimeSearch/AnimeSearch";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
   const [animes, setAnimes] = useState(null)
   const [formData, setFormData] = useState({
     title: ''
   })
+  
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/home')
+  }, [])
 
   const handleSearch = evt => {
     evt.preventDefault()
@@ -29,11 +35,12 @@ function App() {
   }
 
 
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/search/anime' element={<AnimeSearch animes={animes} handleChange={handleChange} handleSearch={handleSearch} />} />
+        <Route path='/home' element={<AnimeSearch animes={animes} handleChange={handleChange} handleSearch={handleSearch} />} />
       </Routes>
     </>
   );
