a549d64b2bd5a32c76c5e9de8b17d7b8401112ae
UbuntuLex <alexandermaniwang99@gmail.com>
2022-08-30 19:01:54 -0400

    add new routes to login and signup page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,13 +4,15 @@ import { searchAnimes } from '../../services/animeService';
 import Navbar from '../../components/Navbar/Navbar';
 import AnimeSearch from "../../components/AnimeSearch/AnimeSearch";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
 
 function App() {
   const [animes, setAnimes] = useState(null)
   const [formData, setFormData] = useState({
     title: ''
   })
-  
+
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -41,6 +43,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/home' />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Signup />} />
         <Route path='/search/anime' element={<AnimeSearch animes={animes} handleChange={handleChange} handleSearch={handleSearch} />} />
       </Routes>
     </>
