c47982c83159ff4594451cba03f50a27432016c3
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-01 17:16:53 -0400

    restyle anime search page

--- src/pages/AnimeSearch/AnimeSearch.jsx
+++ src/pages/AnimeSearch/AnimeSearch.jsx
@@ -5,32 +5,38 @@ import React from 'react'
 export default function AnimeSearch({ animes, handleChange, handleSearch }) {
     return (
         <>
-            <main>
+            <main >
                 <form autoComplete='off' onSubmit={handleSearch}>
                     <input className="input" type="text" name="search" placeholder="Search Anime" autoComplete="off" onChange={handleChange} />
                 </form>
-                {animes ?
-                    <>
-                        {animes.map(anime =>
-                            <div key={anime.id}>
-                                <img src={anime.bannerImage} alt={`${anime.title.romaji}'s banner`} />
-                                <img src={anime.coverImage.large} alt={anime.title.romaji} />
-                                <p>{anime.title.romaji}</p>
-                                <p>Description: {anime.description}</p>
-                                {anime.trailer &&
-                                    <>
-                                        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${anime.trailer.id}M,7U6Y5T43  QA`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                                    </>
-                                }
-                                {!anime.trailer &&
-                                    <p>No Trailer</p>
-                                }
-                            </div>
-                        )}
-                    </>
-                    :
-                    <div>Search Anime!</div>
-                }
+                <div className='flex flex-row flex-wrap'>
+                    {animes ?
+                        <>
+                            {animes.map(anime =>
+                                <div key={anime.id} className='mx-8'>
+                                    {/* <img src={anime.bannerImage} alt={`${anime.title.romaji}'s banner`} /> */}
+                                    <div>
+                                        <img src={anime.coverImage.large} alt={anime.title.romaji} />
+                                        <div className='text-center'>
+                                            <p>{anime.title.romaji}</p>
+                                        </div>
+                                    </div>
+                                    {/* <p>Description: {anime.description}</p> */}
+                                    {/* {anime.trailer &&
+                                        <>
+                                            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${anime.trailer.id}M,7U6Y5T43  QA`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                        </>
+                                    }
+                                    {!anime.trailer &&
+                                        <p>No Trailer</p>
+                                    } */}
+                                </div>
+                            )}
+                        </>
+                        :
+                        <div>Search Anime!</div>
+                    }
+                </div>
             </main>
         </>
     )
