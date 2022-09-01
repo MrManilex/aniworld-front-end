9ab6f273971b602bd2db6ea320ee0347bda3abc9
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-01 15:30:34 -0400

    fix authcontext hook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Home = () => {
-    const { user } = useAuthContext
+    const { user } = useAuthContext()
 
     return (
         <div>
             <h1>Home Page</h1>
-            <p>{ user }</p>
         </div>
     );
 }
