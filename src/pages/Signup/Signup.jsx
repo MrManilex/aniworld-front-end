c20af9b9fd49359e488df8be1a3747b61005b92b
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-01 16:53:45 -0400

    restyle signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -4,6 +4,7 @@ import { useSignup } from '../../hooks/useSignup'
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
     const { signup, error, isLoading } = useSignup()
 
     const handleSubmit = async (e) => {
@@ -13,24 +14,38 @@ const Signup = () => {
     }
 
     return (
-        <form className='signup' onSubmit={handleSubmit}>
-            <h3>Sign up</h3>
+        <form className='text-center flex-col' onSubmit={handleSubmit}>
+            <h2 className="text-4xl m-5">Sign Up</h2>
 
+            <div>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="input input-bordered w-full max-w-xs mt-5"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </div>
+            <div>
+                <input
+                    type="username"
+                    placeholder="Username"
+                    className="input input-bordered w-full max-w-xs my-5"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input input-bordered w-full max-w-xs mb-8"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </div>
 
-            <label>Email:</label>
-            <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-
-            <button disabled={isLoading}>Sign up</button>
+            <button className='btn btn-info' disabled={isLoading}>Sign Up</button>
             {error && <div className="error">{error}</div>}
         </form>
     )
