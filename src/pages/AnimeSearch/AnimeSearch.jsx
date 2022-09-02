4111dd92acc01158dd5f2dc91407bb256679dd5b
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-02 14:21:18 -0400

    restyle anime cards

--- src/pages/AnimeSearch/AnimeSearch.jsx
+++ src/pages/AnimeSearch/AnimeSearch.jsx
@@ -5,22 +5,24 @@ import React from 'react'
 export default function AnimeSearch({ animes, handleChange, handleSearch }) {
     return (
         <>
-            <main >
-                <form autoComplete='off' onSubmit={handleSearch}>
-                    <input className="input" type="text" name="search" placeholder="Search Anime" autoComplete="off" onChange={handleChange} />
-                </form>
+            <main>
+                <div className='text-center my-8'>
+                    <form autoComplete='off' onSubmit={handleSearch}>
+                        <input className="input input-bordered w-full max-w-xs" type="text" name="search" placeholder="Search Anime" autoComplete="off" onChange={handleChange} />
+                    </form>
+                </div>
                 <div className='flex flex-row flex-wrap'>
-                    {animes ?
+                    {animes &&
                         <>
                             {animes.map(anime =>
-                                <div key={anime.id} className='mx-8'>
+                                <div key={anime.id} className='mx-8 mb-2'>
                                     {/* <img src={anime.bannerImage} alt={`${anime.title.romaji}'s banner`} /> */}
-                                    <div>
+                                    {/* <div>
                                         <img src={anime.coverImage.large} alt={anime.title.romaji} />
                                         <div className='text-center'>
                                             <p>{anime.title.romaji}</p>
                                         </div>
-                                    </div>
+                                    </div> */}
                                     {/* <p>Description: {anime.description}</p> */}
                                     {/* {anime.trailer &&
                                         <>
@@ -30,11 +32,19 @@ export default function AnimeSearch({ animes, handleChange, handleSearch }) {
                                     {!anime.trailer &&
                                         <p>No Trailer</p>
                                     } */}
+                                    <div className='w-60'>
+                                        <div className="card bg-base-100 shadow-xl h-auto w-60 ">
+                                            <figure className='min-h-full'>
+                                                <img src={anime.coverImage.large} alt={anime.title.romaji} className='min-w-full h-full' />
+                                            </figure>
+                                        </div>
+                                        <div className='text-center my-2'>
+                                            <h2 className='font-bold'>{anime.title.romaji}</h2>
+                                        </div>
+                                    </div>
                                 </div>
                             )}
                         </>
-                        :
-                        <div>Search Anime!</div>
                     }
                 </div>
             </main>
