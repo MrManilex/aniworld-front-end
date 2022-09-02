85f8fa67b5b3334fbb40b7b72260169251fdbf76
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-02 17:08:08 -0400

    implement an animes release and end dates

--- src/pages/AnimeDetails/AnimeDetails.jsx
+++ src/pages/AnimeDetails/AnimeDetails.jsx
@@ -16,7 +16,20 @@ export default function AnimeDetails() {
                         <p>{anime.title.romaji}</p>
                     </div>
                 </div>
-                <p dangerouslySetInnerHTML={{__html: anime.description}}></p>
+                <p dangerouslySetInnerHTML={{ __html: anime.description }}></p>
+                {/* Fix Dates based on data given */}
+                {anime.startDate.year &&
+                    <>
+                        <h3>Start Date</h3>
+                        <p>{anime.startDate.month}/{anime.startDate.day}/{anime.startDate.year}</p>
+                    </>
+                }
+                {anime.endDate.year &&
+                <>
+                    <h3>End Date</h3>
+                    <p>{anime.endDate.month}/{anime.endDate.day}/{anime.endDate.year}</p>
+                </>
+                }
                 {anime.trailer &&
                     <>
                         <iframe width="560" height="315" src={`https://www.youtube-nocookie.com/embed/${anime.trailer.id}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
