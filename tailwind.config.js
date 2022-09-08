f34e4f1618c2d877d4032ee646831c7d42c80435
Ketchup <alexandermaniwang99@gmail.com>
2022-09-08 16:45:50 -0400

    use custome theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#d1d5db",
+
+          "secondary": "#b45309",
+
+          "accent": "#155e75",
+
+          "neutral": "#1c1917",
+
+          "base-100": "#292524",
+
+          "info": "#22d3ee",
+
+          "success": "#84cc16",
+
+          "warning": "#eab308",
+
+          "error": "#dc2626",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
   content: [,
     "./src/components/**/*",
     "./src/pages/**/*"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
 }
