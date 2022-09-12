bd64f154a8004de39241f00a4db7c643b9e241c7
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-12 14:06:50 -0400

    incorporate user avatar and restyle

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,14 +4,24 @@ export default function Profile({ user }) {
 
     return (
         <>
-            <main>
+            <main className='flex flex-col'>
                 {/* Banner Image Here  */}
-                <div className='flex flex-col text-center'>
-                    {user.profile.avatar &&
-                        <img src={user.profile.avatar} alt="User's Avatar" />
+                <div className='flex flex-row text-center self-center '>
+                    {user.profile.avatar ?
+                        <div className="mx-10 avatar">
+                            <div className="w-24 rounded-xl ">
+                                <img src={user.profile.avatar} alt={`${user.profile.username}'s Avatar`} />
+                            </div>
+                        </div>
+                        :
+                        <label tabIndex="0" className="mx-10 btn btn-ghost btn-circle avatar self-center" style={{ pointerEvents: 'none' }}>
+                            <div className="w-10 rounded-full">
+                                <span className="text-4xl">{user.profile.username[0]}</span>
+                            </div>
+                        </label>
                     }
                     <div>
-                        <h2 className='text-4xl'>{user.profile.username}'s Profile</h2>
+                        <h2 className='text-4xl'>{user.profile.username}'s profile</h2>
                         <p>{user.email}</p>
                     </div>
                 </div>
