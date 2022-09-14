1765a03933f8e97dfa8fc05f398ac4fd616f7b0b
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-14 19:00:05 -0400

    add trending component to search page when use has not searched yet

--- src/pages/AnimeSearch/AnimeSearch.jsx
+++ src/pages/AnimeSearch/AnimeSearch.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Trending from '../../components/Trending/Trending'
 
 export default function AnimeSearch({ animes, handleChange, handleSearch }) {
     return (
@@ -10,28 +11,28 @@ export default function AnimeSearch({ animes, handleChange, handleSearch }) {
                         <input className="input input-bordered w-full max-w-xs" type="text" name="search" placeholder="Search Anime" autoComplete="off" onChange={handleChange} />
                     </form>
                 </div>
-                <div className='flex flex-row flex-wrap place-content-center'>
-                    {animes &&
-                        <>
-                            {animes.map(anime =>
-                                <div key={anime.id} className='mx-8 mb-2'>
-                                    <div className='w-60'>
-                                        <div className="card bg-base-100 shadow-xl h-auto w-60 ">
-                                            <Link to={`/anime/${anime.id}`} state={anime}>
-                                                <figure className='min-h-full'>
-                                                    <img src={anime.coverImage.large} alt={anime.title.romaji} className='min-w-full h-full' />
-                                                </figure>
-                                            </Link>
-                                        </div>
-                                        <div className='text-center my-2'>
-                                            <h2 className='font-bold'>{anime.title.english ? anime.title.english : anime.title.userPreferred}</h2>
-                                        </div>
+                {animes ?
+                    <div className='flex flex-row flex-wrap place-content-center'>
+                        {animes.map(anime =>
+                            <div key={anime.id} className='mx-8 mb-2'>
+                                <div className='w-60'>
+                                    <div className="card bg-base-100 shadow-xl h-auto w-60 ">
+                                        <Link to={`/anime/${anime.id}`} state={anime}>
+                                            <figure className='min-h-full'>
+                                                <img src={anime.coverImage.large} alt={anime.title.romaji} className='min-w-full h-full' />
+                                            </figure>
+                                        </Link>
+                                    </div>
+                                    <div className='text-center my-2'>
+                                        <h2 className='font-bold'>{anime.title.english ? anime.title.english : anime.title.userPreferred}</h2>
                                     </div>
                                 </div>
-                            )}
-                        </>
-                    }
-                </div>
+                            </div>
+                        )}
+                    </div>
+                    :
+                    <Trending />
+                }
             </main>
         </>
     )
