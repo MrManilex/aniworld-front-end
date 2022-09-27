637d8151b36088c8b38e1457247bcd18dbc099d0
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-27 18:59:00 -0400

    stub

--- src/components/CurrentlyWatching/CurrentlyWatching.jsx
+++ src/components/CurrentlyWatching/CurrentlyWatching.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 export default function CurrentlyWatching() {
+    const [current, setCurrent] = useState(null)
+
+    useEffect(() => {
+        // call function to retrieve logged in user's currently watching list
+    }, [])
+
     return (
-        <div>CurrentlyWatching</div>
+        <>
+            <h1>Currently Watching</h1>
+            {/* for each anime watching display for current user */}
+        </>
     )
 }
