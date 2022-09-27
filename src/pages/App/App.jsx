19d3e1f56b479f8d6cbf99e23d9c9bebb44de069
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-27 19:00:55 -0400

    pass state through Login page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -54,7 +54,7 @@ function App() {
       <Navbar handleLogout={handleLogout} user={user} />
       <Routes>
         <Route path='/home' element={user ? <Home setAnimes={setAnimes} /> : <Navigate to='/login' />} />
-        <Route path='/login' element={!user ? <Login /> : <Navigate to='/home' />} />
+        <Route path='/login' element={!user ? <Login setAnimes={setAnimes}/> : <Navigate to='/home' />} />
         <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/home' />} />
 
         {/* Profile Section */}
